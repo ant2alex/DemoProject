7d2f94e7417e0ce933b0d0e57e744ec57dd937d7
Eddie <Cibermon@web.de>
2018-04-09 13:50:39 +0200

    Random Food Spawns
    Added Random Food Spawn hehe

---
+++ Snake3D/Assets/Scripts/Food.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+
+    public GameObject FoodPrefab;
+
+    public Transform topBorder;
+    public Transform botBorder;
+    public Transform leftBorder;
+    public Transform rightBorder;
+
+    [SerializeField] float time;
+    [SerializeField] float repeatRate;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        InvokeRepeating("Spawn",time,repeatRate);
+	}
+
+    void Spawn()
+    {
+        int z = (int)Random.Range(botBorder.position.z,topBorder.position.z);
+
+        int x = (int)Random.Range(leftBorder.position.x, rightBorder.position.x);
+
+        Instantiate(FoodPrefab, new Vector3(x, 0, z), Quaternion.identity);
+    }
+}
