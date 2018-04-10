2ed072ead77530d44b2a91d5888b9797769fa11c
BeneDev <seriousbenedikt@web.de>
2018-04-10 07:48:30 -0200

    Changed font detail in main menu

--- Snake3D/Assets/Scripts/PickUpController.cs
+++ Snake3D/Assets/Scripts/PickUpController.cs
@@ -4,25 +4,19 @@ using UnityEngine;
 
 public class PickUpController : MonoBehaviour {
 
-    [SerializeField] GameObject snake;
+    GameObject snake;
 
-    //private void Awake()
-    //{
-    //    GameObject.FindGameObjectWithTag("Player");
-    //}
+    private void Awake()
+    {
+        GameObject.FindGameObjectWithTag("Player");
+    }
 
     private void Update()
     {
         if (transform.position == snake.GetComponent<SnakeController>().Entities.First.Value.transform.position)
         {
-            print("Expand");
             snake.GetComponent<SnakeController>().Expand();
+            Destroy(gameObject);
         }
     }
-
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    print("Expand");
-    //    snake.GetComponent<SnakeController>().Expand();
-    //}
 }
